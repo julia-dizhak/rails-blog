817e45c2e6c97f98fba5b278dd4994667abce565
Julia Dizhak <julia.dizhak@teamtailor.com>
2023-09-20 14:09:02 +0200

    added create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,12 +5,30 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-  # rescue ActiveRecord::RecordNotFound
-  #   redirect_to root_path 
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end  
 
   # create but does not save it
   def new
     @article = Article.new
+  end
+
+  def create
+    # @article = Article.new(params[:article])
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # use strong params
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/articles", to: "articles#index"
+  get "/articles/new", to: "articles#new"
+  get "/articles/:id", to: "articles#show"
+
+  post "/articles", to: "articles#create"
+
   # Defines the root path route ("/")
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-  get "/articles/new", to: "articles#new"
-
-  
   resources :articles
 end
